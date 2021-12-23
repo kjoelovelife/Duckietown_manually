1662bd4188d9b934ecbd56b59c1a5fcbcf08931d
Joe Lin <weichih.lin@protonmail.com>
2021-12-23 10:23:50 +0800

    Fix: Edit contents in environment.sh
    Problem:
      1. Can not find the workspace "duckietown/DB21M".
      2. Can not start the communication with the duckiebot via the ROS.
    
    Reason:
      1. Rename the project "Duckietown_manually".
      2. Do not set the ROS_MASTER_URI and ROS_HOSTNAME.
    
    Adjust item:
      1. Add variable "current_ip" with line 3
      2. Modified variable "workspace" from "$HOME/duckietown" to "$HOME/Duckietown/DB21M" with line 4
      3. Add external variable ROS_MASTER_URI with line 15 and 19
      4. Add external variable ROS_MASTER_URI with line 15 and 19

--- DB21M/setup/environment.sh
+++ DB21M/setup/environment.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
-export workspace=$HOME/duckietown
+current_ip=$(hostname -I | awk '{print $1}')
+export workspace=$HOME/Duckietown_manually/DB21M
 echo "Setting up PYTHONPATH."
 export PYTHONPATH=$workspace/catkin_ws/src:$PYTHONPATH
 
-echo "Source workspace \"duckietown\"."
-source ~/duckietown/DB21M/catkin_ws/devel/setup.bash
+echo "Source workspace \"$workspace\"."
+source ~/Duckietown_manually/DB21M/catkin_ws/devel/setup.bash
 
 echo "Setting VEHICLE_NAME..."
 if [ $# -gt 0 ]; then
 	# provided a hostname, use it as ROS_MASTER_URI
 	export VEHICLE_NAME=$1
+	export ROS_MASTER_URI=http://$1.local:11311
 else
 	echo "No hostname provided. Using $HOSTNAME."
 	export VEHICLE_NAME=$HOSTNAME
+	export ROS_MASTER_URI=http://$current_ip:11311
 fi
 echo "VEHICLE_NAME set to $VEHICLE_NAME"
+echo "ROS_MASTER_URI set to $ROS_MASTER_URI"
+echo "ROS_HOSTNAME set to $current_ip"
 echo ""
